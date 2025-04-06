daf708a48d2ec9b0a9653f4df96be7f380593243
Zain Malik <79419086+ZainMalik001@users.noreply.github.com>
2025-04-07 01:47:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from langchain_community.tools import WikipediaQueryRun, ArxivQueryRun, DuckDuck
 from langchain_community.utilities import WikipediaAPIWrapper, ArxivAPIWrapper
 from langchain.agents import initialize_agent, AgentType
 
-####
-#
 st.title("LangChain - Search with langchain")
 
 api_key = st.text_input("Write GROQ API Key", type="password")
